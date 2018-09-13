9fabc20a4468352c312c45270cda41c1bb5a99e8
manuelnelson <elnels@gmail.com>
2018-09-13 10:13:50 -0400

    data4sdgs permission moved before check

--- app/src/routes/api/v1/datasetRouter.js
+++ app/src/routes/api/v1/datasetRouter.js
@@ -106,12 +106,12 @@ class DatasetRouter {
 }
 
 const containApps = function (apps1, apps2) {
-    if (!apps1 || !apps2) {
-        return false;
-    }
     //admin user for data4sdgs
     if(apps2.indexOf('data4sdgs') > -1)
         return true;
+    if (!apps1 || !apps2) {
+        return false;
+    }
     for (let i = 0, length = apps1.length; i < length; i++)  {
         for (let j = 0, length2 = apps2.length; j < length2; j++) {
             if (apps1[i] === apps2[j]) {
